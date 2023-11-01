e5aaa2083d0480f777be27b40df0055d2574b397
Manuel Gomez <magomez36@utpl.edu.ec>
2023-11-01 16:30:33 -0500

    Manuel Gomez

---
+++ APE_Semana4_Soluciones/src/ape_semana4_soluciones/AreaTriangulo_1.java
@@ -0,0 +1,21 @@
+/*
+-Algoritmo-1
+Generar un algoritmo que permite calcular y presentar el área de un triángulo. Los datos deben ser pedidos al usuario.
+ */
+package ape_semana4_soluciones;
+import java.util.Scanner;
+public class AreaTriangulo_1 {
+    public static void main(String[] args) {
+        double baseTriangulo;
+        double alturaTriangulo;
+        double areaTriangulo;
+        Scanner teclado = new Scanner(System.in);//Inicializar
+        System.out.println("Ingrese la base del Triangulo");
+        baseTriangulo = teclado.nextDouble();
+        System.out.println("Ingrese la altura del Triangulo");
+        alturaTriangulo = teclado.nextDouble();
+        areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
+        System.out.println("El area del trianguloes: " + areaTriangulo);
+        
+    }
+}
